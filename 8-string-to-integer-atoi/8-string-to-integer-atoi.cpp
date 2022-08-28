2e7d0d22827acbba798172d73ae730556c401834
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2022-08-29 03:07:07 +0530

    Time: 9 ms (23.27%), Space: 7 MB (53.85%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,76 +1,62 @@
-class Solution {
+class Solution
+{
 public:
-    int myAtoi(string s) {
+    int myAtoi(string s)
+    {
         string temp = "";
         long long num = 0;
-        int i=0;
-        
-        while(i<s.length()){
-            while(s[i]==' '){
+        int i = 0;
+
+        while (i < s.length()) {
+            while (s[i] == ' ') {
                 i++;
             }
-            if((s[i]=='+' || s[i]=='-')&&!(s[i+1]>='0' && s[i+1]<='9')){
-                return num; 
-            }
-            while((s[i]>='0' && s[i]<='9') || (s[i]=='+' || s[i]=='-')){
-             
-                if((s[i]=='+' || s[i]=='-') && temp.length()>=1){
+            if ((s[i] == '+' || s[i] == '-') && !(s[i + 1] >= '0' && s[i + 1] <= '9'))
+                return num;
+
+            while ((s[i] >= '0' && s[i] <= '9') || (s[i] == '+' || s[i] == '-')) {
+                if ((s[i] == '+' || s[i] == '-') && temp.length() >= 1)
                     break;
-                }
+
                 temp.push_back(s[i]);
                 i++;
             }
             break;
         }
-        
-        cout<<temp<<endl;
-
         int neg = 0;
-        if(temp[0]=='-'){
+        if (temp[0] == '-') {
             neg = 1;
             temp.erase(temp.begin());
         }
-        else if(temp[0]=='+'){
+        else if (temp[0] == '+')
             temp.erase(temp.begin());
+        
+        i = 0;
+        while (temp[i] == '0')    {
+            i++;
         }
         
-        int ind = 0;
-        while(temp[ind]=='0'){
-            ind++;
-        }
-        
-        temp.erase(0,ind);      
-        
-        cout<<temp;
+        temp.erase(0, i);
         long long power = 1;
         int digit;
-        
-        for(int i=temp.length()-1; i>=0;i--){
-            if(temp[i]>='0' && temp[i]<='9'){
+
+        for (int i = temp.length() - 1; i >= 0; i--)    {
+            if (temp[i] >= '0' && temp[i] <= '9')   {
                 digit = temp[i] - '0';
                 num += (digit * power);
                 
-                if((num>INT_MAX)||(power>INT_MAX)){
-                    if(neg){
+                if ((num > INT_MAX) || (power > INT_MAX))   {
+                    if (neg)
                         return INT_MIN;
-                    }
-                    else{
+                    else
                         return INT_MAX;
-                    }
                 }
-                
                 power *= 10;
-                
-                
             }
         }
-        
-        if(neg){
+        if (neg)
             num -= num * 2;
-        }
-            
 
-        
         return num;
     }
 };
