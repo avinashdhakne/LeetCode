691a3821c292f2e67d00c2f95e0dec19e2743ad9
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2022-08-28 09:26:40 +0530

    Time: 36 ms (8.62%), Space: 10.6 MB (5.01%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,65 +1,36 @@
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> diagonalSort(vector<vector<int>> &mat)
-    {
+    vector<vector<int>> diagonalSort(vector<vector<int>> &mat)  {
         int m = mat.size();
         int n = mat[0].size();
-        
-        for(int i=0; i<m; i++){
-            swap(mat,i,0);
-        }
-        
-        for(int j=1; j<n; j++){
-            swap(mat, 0,j);
-        }
-        
+
+        for (int i = 0; i < m; i++) swap(mat, i, 0);
+        for (int j = 1; j < n; j++) swap(mat, 0, j);
+
         return mat;
     }
-    
-    
-    void swap(vector<vector<int>> &mat, int row, int col){
+
+    void swap(vector<vector<int>> &mat, int row, int col)   {
         int m = mat.size();
         int n = mat[0].size();
-        map<int,int>map;
-        
+        map<int, int> map;
+
         int i = row;
         int j = col;
-        
-        while(i<m && j<n){
-            map[mat[i][j]]++;
-            j++;
-            i++;
-        }
-        
-        // auto itr = map.begin();
-        i = row;
-        j = col;
-        
-//         for(auto &m: map){
-//             cout<<m.first<<" "<<m.second<<endl;
-//         }
-//         cout<<endl;
 
-        // while(i<m && j<n){
-        //     if((*itr).second > 0){
-        //         mat[i][j] = (*itr).first;
-        //         (*itr).second--;
-        //     }
-        //     itr++;
-        //     j++;
-        //     i++;
-        // }
-        
-        for (auto itr = map.begin(); itr != map.end(); itr++)
-        {   
-            while ((*itr).second > 0)
-            {
+        while (i < m && j < n)  {
+            map[mat[i][j]]++;
+            j++;    i++;
+        }
+
+        i = row;    j = col;
+        for (auto itr = map.begin(); itr != map.end(); itr++)   {
+            while ((*itr).second > 0)   {
                 mat[i][j] = (*itr).first;
                 (*itr).second--;
-                i++;
-                j++;
+                i++;    j++;
             }
         }
-    }   
-    
+    }
 };
