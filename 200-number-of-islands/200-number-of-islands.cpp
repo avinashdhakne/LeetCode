dfe9bcefc387063d94c21ce668ae1442d5405246
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2022-08-29 21:34:21 +0530

    Time: 51 ms (60.79%), Space: 19.2 MB (9.36%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,59 @@
+// Reference: https://www.youtube.com/watch?v=muncqlKJrH0
+class Solution
+{
+    void bfs(vector<vector<char>> &grid, vector<vector<int>> &visited, int row, int col)
+    {
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        visited[row][col] = 1;
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        while (!q.empty())
+        {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for (int delrow = -1; delrow <= 1; delrow++)
+            {
+                for (int delcol = -1; delcol <= 1; delcol++)
+                {
+                    int nrow = row + delrow;
+                    int ncol = col + delcol;
+                    if(!((delrow==1 && delcol==1) || (delrow==-1 && delcol==-1) ||
+                       (delrow==-1 && delcol==1) || (delrow==1  && delcol==-1))){
+                        if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&                                    visited[nrow][ncol] == 0 && grid[nrow][ncol] == '1')
+                    {
+                        visited[nrow][ncol] = 1;
+                        q.push({nrow, ncol});
+                    }
+                    }
+                }
+            }
+        }
+    }
+
+public:
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> visited(m, vector<int>(n, 0));
+        int count = 0;
+
+        for (int row = 0; row < m; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                if (!visited[row][col] && grid[row][col]=='1')
+                {
+                    count++;
+                    bfs(grid, visited, row, col);
+                }
+            }
+        }
+        return count;
+    }
+};
