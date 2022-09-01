f06d4af7b1702aff876225ce70d45aeaae2f7849
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2022-09-01 19:03:52 +0530

    Time: 54 ms (57.64%), Space: 71.5 MB (51.52%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,93 +11,26 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *temp = l1;
-        int addCurr = 0, addNext = 0; int result;
-        while(temp!=NULL && l2!=NULL) {
-          
-                // cout<<"temp: "<<temp->val<<" l2: "<<l2->val<<" addnext: "<<addNext<<endl;
-                result = temp->val + l2->val + addNext;
-                temp->val = result % 10;
-                // cout<<"new temp: "<<temp->val<<endl;
-
-                if(result >= 10) {
-                    addNext = 1;
-                    // cout<<"added now:"<<endl;
-                }
-                else 
-                    addNext = 0;
-                // cout<<"----------------------------"<<endl;
-            
-            
-            if(temp->next == NULL && l2->next!=NULL){
-                cout<<"temp: "<<endl;
-                l2 = l2->next;
-                
-                while(l2!=NULL){
-                    result = l2->val + addNext;
-                    cout<<"Result: "<<result%10<<endl;
-                    ListNode * newNode = new ListNode(result%10);
-                    temp->next = newNode;
-                    if(result >= 10) {
-                        cout<<"yes";
-                        addNext = 1;  
-                    }    
-                    else 
-                        addNext = 0;
-                    if(l2->next == NULL && addNext == 1){
-                        temp = temp->next;
-                        ListNode *remainder = new ListNode(1);
-                        temp->next = remainder;
-                        break;
-                    }
-                    else{
-                        temp = temp->next;
-                        l2 = l2->next;
-                    }
-                }
-                break;
-
+        ListNode  *temp = new ListNode();
+        ListNode *iter = temp;
+        int carry = 0;
+        while(l1!=NULL || l2!=NULL || carry){
+            int sum = 0;
+            if(l1!=NULL){
+                sum+=l1->val;
+                l1 = l1->next;
+            }
+            if(l2!=NULL){
+                sum+=l2->val;
+                l2=l2->next;
             }
             
-            if(l2->next==NULL && temp->next!=NULL){
-                cout<<"l2"<<endl;
-                temp = temp->next;
-
-                while(temp!=NULL){
-                    cout<<"temp: "<<temp->val<<" ";
-                    result = temp->val + addNext;
-                    cout<<"Result: "<<result<<endl;
-
-                    temp->val = result%10;
-                    if(result >= 10) 
-                        addNext = 1;
-                    else 
-                        addNext = 0;
-                    if(temp->next == NULL && addNext == 1){
-                        ListNode *remainder = new ListNode(1);
-                        temp->next = remainder;
-                        break;
-                    }
-                    else{
-                        temp = temp->next;
-                    }
-                }
-                
-                break;
-            }
-            
-           if(temp->next == NULL && addNext == 1){
-                ListNode *remainder = new ListNode(1);
-                temp->next = remainder;
-                    break;
-            }
-            else{
-                temp = temp->next;
-                l2 = l2->next;
-            }  
+            sum+=carry;
+            ListNode *node = new ListNode(sum%10);
+            iter->next = node;
+            carry = sum/10;
+            iter = iter->next;
         }
-        
-       
-        return l1;
+        return temp->next;
     }
 };
