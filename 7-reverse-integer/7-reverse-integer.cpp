36a162fc405dba7c21ad5a07f15cd817ff6c4099
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2022-09-02 22:50:36 +0530

    Time: 3 ms (64.69%), Space: 6.2 MB (5.86%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int reverse(int x) {
+        vector<int> digit;
+        long result = 0;
+        long power = 1;
+        
+        while(x!=0){
+            digit.push_back(x%10);
+            x/=10;
+        }
+        
+        for(int i=digit.size()-1; i>=0;i--){
+            result += digit[i] * power;
+            cout<<result;
+            power *= 10; 
+        }
+        
+        if(result > INT_MAX || result < INT_MIN)
+            return 0;
+        
+        return result;
+    }
+};
