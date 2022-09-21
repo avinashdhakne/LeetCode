3e8d2f3f28ea9d872bf72a0bc513465e9f454b62
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2022-09-21 18:08:31 +0530

    Time: 126 ms (83.93%), Space: 46.6 MB (25.00%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -15,16 +15,13 @@ public:
         int sum = evenSum(nums);
         
         for(int i=0; i<queries.size(); i++){
-            
-            if(nums[queries[i][1]]%2==0){
+            if(nums[queries[i][1]]%2==0) {
                 sum = sum - nums[queries[i][1]];
             }
-
             nums[queries[i][1]] = nums[queries[i][1]] + queries[i][0];
-            if(nums[queries[i][1]]%2==0){
+            if(nums[queries[i][1]]%2==0) {
                 sum += nums[queries[i][1]];
             }
-
             result.push_back(sum);
         }
         return result;
