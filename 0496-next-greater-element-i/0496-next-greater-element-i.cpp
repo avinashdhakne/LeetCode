32859d317248b8432a80eba4b688ee81b734ef59
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2022-11-09 11:38:21 +0530

    Time: 20 ms (25.34%), Space: 8.4 MB (87.22%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findNextGreaterElement(int num, vector<int> &nums){
+        int i=0;
+        while(nums[i]!=num && i<nums.size()){
+            i++;
+        }
+        
+        int result = -1;
+        for(int j=i+1; j<nums.size(); j++){
+            if(nums[j] > num){
+                result = nums[j];
+                break;
+            }
+        }
+        cout<<endl;
+        return result;
+    }
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        for(int num: nums1){
+           result.push_back(findNextGreaterElement(num,nums2));
+        }
+        
+        return result;
+    }
+};
