291fcba3d36b7c82409bc0f11396cf07c72e0f33
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2023-01-15 12:23:02 +0530

    Time: 34 ms (92.95%), Space: 89.7 MB (41.78%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -5,11 +5,13 @@ public:
         int n2 = nums2.size();
         
         vector<int> merge(n1+n2);
-        
+
         int a = 0;
         int b = 0;
         int i = 0;
     
+        
+        
         while(a < n1 && b < n2)
         {
             if(nums1[a] < nums2[b]){
@@ -23,16 +25,17 @@ public:
             i++;  
         }
         
-        while(a < n1){
+        while(a <= n1/2 && n1!=0){
             merge[i] = nums1[a];
             a++; i++;
         }
         
-        while(b < n2){
+        while(b <= n2/2  && n2!=0){
             merge[i] = nums2[b];
             b++; i++;
         }
-            
+        
+        
        
         if(merge.size() % 2 == 0){
            return double (merge[merge.size()/2] + merge[merge.size()/2 - 1])/2;
