bf16e8181eda23d9e1efec47e8aa497b38ccaa0b
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2023-01-25 00:04:14 +0530

    Time: 744 ms (5.15%), Space: 363.1 MB (5.58%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -1,48 +1,59 @@
 class Solution {
-public:  
-      vector<int> findcordinates(int pos,int n){
-          vector<int>temp;
-            int r=n-(pos-1)/n-1;
-            int c=(pos-1)%n;
-            if(r%2==n%2){
-                return temp={r,n-1-c};
-            }else{
-                return temp={r,c};
-            }
-            }
-
-
+public:
+  
     int snakesAndLadders(vector<vector<int>>& board) {
-        int n= board.size();
-        queue<int>q;
-        vector<vector<bool>>visited( n , vector<bool>(n, false)); 
-        int ans=0;
+        int n = board.size();
+        vector<vector<int>> v(n,vector<int> (n));
+        
+        queue<int> q;
         q.push(1);
-        visited[n-1][0]=true;
+        v[n-1][0] = 1;
+        int count = 0;
         while(!q.empty()){
-            int size=q.size();
-           
-            for(int i=0;i<size;++i){
-                int front=q.front();
+            int size = q.size();
+            
+            
+            
+            for(int j=0; j<size; j++){
+                int pos = q.front();
                 q.pop();
-                if(front==n*n) return ans;
-                for(int k=1;k<=6;++k){
-                    if(k+front>n*n) break;
-                    vector<int>pos=findcordinates(front+k,n);
-                    int r=pos[0];
-                    int c=pos[1];
-                    if(visited[r][c]==true) continue;
-                    visited[r][c]=true;
-                    if(board[r][c]!=-1){
-                        q.push(board[r][c]);
-                    }else{
-                        q.push(front+k);
-                    }
-
+                cout<<pos<<" ";
+                if(pos == pow(n,2)) return count;
+                
+                int minpos = minimum(pos+6, pow(n,2));
+                for(int i = pos+1; i <= minpos; i++){
+                    pair<int,int> cord = convertPos(board, i);
+                    int row = cord.first;
+                    int col = cord.second;
+                    
+                    if((board[row][col] != -1) && v[row][col] == 0)
+                        q.push(board[row][col]);
+                    else if(v[row][col] == 0)
+                        q.push(i);
+                    v[row][col] = 1;
                 }
             }
-            ans++;
+            cout<<endl;
+            count++;
         }
+        
         return -1;
+    }
+    
+     pair <int, int> convertPos(vector<vector<int>> board, int pos){
+        int n = board.size();
+        pos = pow(n,2) - pos;
+        
+        int col, row = (pos/ (n));
+        if(((n-1) - row) % 2 == 1)
+            col = pos - row * n; 
+        else
+            col = (n-1) - (pos - (row * n));
+        return {row,col};
+    }
+    
+    int minimum(int a, int b){
+        if(a>b) return b;
+        return a;
     }
 };
