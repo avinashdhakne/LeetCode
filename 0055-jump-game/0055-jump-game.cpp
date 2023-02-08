d218f3670020ef7bb70d8c920abe12823141330a
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2023-02-08 22:35:43 +0530

    Time: 56 ms (95.76%), Space: 48.5 MB (47.91%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,14 +3,9 @@ public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
         int index = n-1;
-        
-        for(int i=(n-2); i>=0; i--){
-            for(int j=i+1; j<=(i+nums[i]); j++){
-                if(j==index){ 
-                    index = i;
-                    break;
-                }
-            }
+        for(int i=(n-1); i>=0; i--){
+            if(index <= nums[i]+i)
+                index = i;
         }
         
         if(index!=0)
