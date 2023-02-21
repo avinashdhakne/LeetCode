6b9f7f343435763ef339b7104c7442b4f0416991
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2023-02-21 15:09:47 +0530

    Time: 27 ms (74.00%), Space: 33 MB (6.23%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,9 +1,31 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int result = 0;
-        for(auto i: nums)
-            result = result ^ i;
-        return result;
+        if(nums.size() <= 1)
+            return nums[0];
+        return nums[findelement(nums, 0, nums.size()-1)];
+    }
+    
+    int findelement(vector<int> nums, int s, int e){
+        int mid = s + ((e - s)/2);
+        cout<<mid<<endl;
+        if(mid == 0){
+            if(nums[mid] != nums[mid+1])
+                return mid;
+        }
+        
+        if(mid == nums.size()-1){
+            if(nums[mid] != nums[mid-1]){
+                return mid;
+            }
+        }
+        if((nums[mid + 1] != nums[mid]) && (nums[mid - 1] != nums[mid]))
+            return mid;
+        else if(!(mid&1) && nums[mid] == nums[mid+1])
+            return findelement(nums, mid+1, e);
+        else if((mid&1) && nums[mid] == nums[mid-1])
+            return findelement(nums, mid+1, e);
+        else 
+            return findelement(nums, s, mid-1);
     }
 };
