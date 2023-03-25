4ba5453c266418ee384fc81a302d6c0ee1a29ba2
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2023-03-25 13:12:15 +0530

    Time: 491 ms (36.62%), Space: 0B (100.00%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,3 +1,7 @@
 # Write your MySQL query statement below
 UPDATE salary
-SET sex = CHAR(ASCII('f') ^ ASCII('m') ^ ASCII(sex));
+SET sex = 
+(CASE 
+ WHEN sex='f' THEN 'm'
+ WHEN sex='m' THEN'f'
+END);
