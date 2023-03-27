ff895ee67ad16e60638f9eb84b5188b4eba68811
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2023-03-27 14:39:06 +0530

    Time: 211 ms (5.30%), Space: 10.2 MB (7.26%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        
+        for(int i=1; i<cols; i++){
+            grid[0][i] += grid[0][i-1];
+        }
+        
+        for(int i=1; i<rows; i++){
+            grid[i][0] += grid[i-1][0];
+        }
+        
+         for(auto i: grid){
+            for(auto j: i){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+        
+        for(int i=1; i<rows; i++){
+            for(int j=1; j<cols; j++){
+                int left = grid[i][j] + grid[i][j-1];
+                int right = grid[i][j] + grid[i-1][j];
+                cout<<grid[i][j]<<"+"<<grid[i][j-1]<<"/"<<grid[i-1][j]<<" ";
+                grid[i][j] = min(left, right);
+            }
+            cout<<endl;
+        }
+        
+         for(auto i: grid){
+            for(auto j: i){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+        
+        return grid[rows-1][cols-1];  
+    }
+};
