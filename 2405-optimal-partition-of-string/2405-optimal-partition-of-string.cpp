cd7f6ba2548b5033e9cbe923f6841e2ab23b2d66
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2023-04-04 10:18:16 +0530

    Time: 47 ms (75.72%), Space: 31.2 MB (68.54%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -5,29 +5,20 @@ public:
         vector<string> strs;
         string temp;
         for(auto i: s){
-            if(!check[i-'a']){
-                temp.push_back(i);
-                check[i-'a'] = 1;
-            }
-            else{
-                if(temp != ""){
+            if(check[i-'a']){
+                if(temp != "")
                     strs.push_back(temp);
-                }
                 temp = "";
-                for(int i=0; i<26; i++){
+                for(int i=0; i<26; i++)
                     check[i] = 0;
-                }
+            }
                 temp.push_back(i);
                 check[i-'a'] = 1;
-            }
         }
+        
         if(temp != ""){
-                    strs.push_back(temp);
-                }
-            
-            for(auto i: strs){
-                cout<<i<<" ";
-            }
+            strs.push_back(temp);
+        }
         return strs.size();
     }
 };
